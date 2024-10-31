e8168af3d7cbe02e0ee96c4569938c833b51843e
AoiKamishiro <40717483+AoiKamishiro@users.noreply.github.com>
2024-10-31 19:40:49 +0900

    chor: Unity2022対応

--- Editor/SubAssetEditor.cs
+++ Editor/SubAssetEditor.cs
@@ -1,9 +1,9 @@
 using System.Collections.Generic;
-using UnityEngine;
-using UnityEditor;
-using System.Linq;
 using System.IO;
+using System.Linq;
+using UnityEditor;
 using UnityEditor.Animations;
+using UnityEngine;
 
 namespace Coffee.Editors
 {
@@ -15,20 +15,19 @@ namespace Coffee.Editors
         private static GUIContent _contentImport;
         private static GUIContent _contentExport;
         private static bool _cached = false;
-        const float ICON_SIZE = 20;
+        private const float ICON_SIZE = 20;
 
         private bool _isLocked;
         private bool _isRenaming;
         private bool _hasSelectionChanged;
         private Object _current;
         private Vector2 _scrollPosition;
-        private List<Object> _subAssets = new List<Object>();
-        private readonly List<Object> _referencingAssets = new List<Object>();
+        private List<Object> _subAssets = new();
+        private readonly List<Object> _referencingAssets = new();
 
         private static void CacheGUI()
         {
-            if (_cached)
-                return;
+            if (_cached) return;
             _cached = true;
 
             _contentNoRef = new GUIContent(EditorGUIUtility.FindTexture("console.warnicon.sml"), "Not referenced by main asset");
@@ -41,7 +40,7 @@ namespace Coffee.Editors
         [MenuItem("Assets/Sub Asset Editor")]
         public static void OnOpenFromMenu()
         {
-            EditorWindow.GetWindow<SubAssetEditor>("Sub Asset");
+            GetWindow<SubAssetEditor>("Sub Asset");
         }
 
         private void OnEnable()
@@ -57,8 +56,8 @@ namespace Coffee.Editors
         private void OnSelectionChanged()
         {
             // On select new asset.
-            var active = Selection.activeObject;
-            if (!_isLocked && active && _current != active && !(active is SceneAsset) && AssetDatabase.IsMainAsset(active))
+            Object active = Selection.activeObject;
+            if (!_isLocked && active && _current != active && active is not SceneAsset && AssetDatabase.IsMainAsset(active))
             {
                 OnSelectionChanged(active);
             }
@@ -69,7 +68,7 @@ namespace Coffee.Editors
             _current = active;
 
             // Find sub-assets.
-            var assetPath = AssetDatabase.GetAssetPath(active);
+            string assetPath = AssetDatabase.GetAssetPath(active);
             _subAssets = AssetDatabase.LoadAllAssetsAtPath(assetPath)
                 .Where(x => x != null && !(x is GameObject || x is Component) && x != _current && 0 == (x.hideFlags & HideFlags.HideInHierarchy))
                 .Distinct()
@@ -77,14 +76,14 @@ namespace Coffee.Editors
 
             // Find referencing assets.
             _referencingAssets.Clear();
-            foreach (var o in AssetDatabase.LoadAllAssetsAtPath(AssetDatabase.GetAssetPath(active)))
+            foreach (Object o in AssetDatabase.LoadAllAssetsAtPath(AssetDatabase.GetAssetPath(active)))
             {
                 if (o == null)
                 {
                     continue;
                 }
 
-                var sp = new SerializedObject(o).GetIterator();
+                SerializedProperty sp = new SerializedObject(o).GetIterator();
                 sp.Next(true);
 
                 // Search referencing asset in SerializedProperties.
@@ -92,7 +91,7 @@ namespace Coffee.Editors
                 {
                     if (sp.propertyType != SerializedPropertyType.ObjectReference || !sp.objectReferenceValue) continue;
 
-                    var asset = sp.objectReferenceValue;
+                    Object asset = sp.objectReferenceValue;
                     if (!(asset is GameObject || asset is Component) && active != asset && o != asset && 0 == (asset.hideFlags & HideFlags.HideInHierarchy) && !_referencingAssets.Contains(asset))
                     {
                         _referencingAssets.Add(asset);
@@ -106,7 +105,7 @@ namespace Coffee.Editors
 
         private void DeleteSubAsset(Object asset)
         {
-            Object.DestroyImmediate(asset, true);
+            DestroyImmediate(asset, true);
             _hasSelectionChanged = true;
         }
 
@@ -122,24 +121,24 @@ namespace Coffee.Editors
         {
             // accepted object
             assets = assets
-                .Where(x => x != _current && !(x is SceneAsset) && AssetDatabase.Contains(x) && !_subAssets.Contains(x));
+                .Where(x => x != _current && x is not SceneAsset && AssetDatabase.Contains(x) && !_subAssets.Contains(x));
 
             // Replace targets
-            var replaceTargets = AssetDatabase.LoadAllAssetsAtPath(AssetDatabase.GetAssetPath(_current))
+            SerializedObject[] replaceTargets = AssetDatabase.LoadAllAssetsAtPath(AssetDatabase.GetAssetPath(_current))
                 .Select(x => new SerializedObject(x))
                 .ToArray();
 
             //
-            foreach (var asset in assets)
+            foreach (Object asset in assets)
             {
-                var newInstance = Object.Instantiate(asset);
+                Object newInstance = Instantiate(asset);
                 newInstance.name = asset.name;
                 AssetDatabase.AddObjectToAsset(newInstance, _current);
 
                 // Find referencing assets.
-                foreach (var so in replaceTargets)
+                foreach (SerializedObject so in replaceTargets)
                 {
-                    var sp = so.GetIterator();
+                    SerializedProperty sp = so.GetIterator();
 
                     // Search referencing asset in SerializedProperties.
                     while (sp.Next(true))
@@ -165,10 +164,10 @@ namespace Coffee.Editors
 
             using (new EditorGUILayout.HorizontalScope())
             {
-                var rLabel = EditorGUILayout.GetControlRect(GUILayout.Width(80));
+                Rect rLabel = EditorGUILayout.GetControlRect(GUILayout.Width(80));
                 GUI.Toggle(rLabel, true, "<b>Main Asset</b>", "IN Foldout");
 
-                var rLock = EditorGUILayout.GetControlRect(GUILayout.Width(20));
+                Rect rLock = EditorGUILayout.GetControlRect(GUILayout.Width(20));
                 rLock.y += 2;
                 if (GUI.Toggle(rLock, _isLocked, GUIContent.none, "IN LockButton") != _isLocked)
                 {
@@ -181,7 +180,7 @@ namespace Coffee.Editors
             EditorGUI.indentLevel++;
             using (new EditorGUILayout.HorizontalScope())
             {
-                var r = EditorGUILayout.GetControlRect(true);
+                Rect r = EditorGUILayout.GetControlRect(true);
 
                 r.width -= 20;
                 EditorGUI.ObjectField(r, _current, _current.GetType(), false);
@@ -195,7 +194,7 @@ namespace Coffee.Editors
                 }
             }
 
-            using (EditorGUILayout.ScrollViewScope scrollView = new EditorGUILayout.ScrollViewScope(_scrollPosition))
+            using (EditorGUILayout.ScrollViewScope scrollView = new(_scrollPosition))
             {
                 _scrollPosition = scrollView.scrollPosition;
                 EditorGUI.indentLevel--;
@@ -203,10 +202,10 @@ namespace Coffee.Editors
                 GUILayout.Space(10);
                 using (new EditorGUILayout.HorizontalScope())
                 {
-                    var rLabel = EditorGUILayout.GetControlRect(GUILayout.Width(80));
+                    Rect rLabel = EditorGUILayout.GetControlRect(GUILayout.Width(80));
                     GUI.Toggle(rLabel, true, "<b>Sub Asset</b>", "IN Foldout");
 
-                    var rRename = EditorGUILayout.GetControlRect(GUILayout.Width(60));
+                    Rect rRename = EditorGUILayout.GetControlRect(GUILayout.Width(60));
                     _isRenaming = GUI.Toggle(rRename, _isRenaming, "Rename", EditorStyles.miniButton);
                     GUILayout.FlexibleSpace();
                     if (GUILayout.Button("Show All"))
@@ -225,12 +224,12 @@ namespace Coffee.Editors
                 }
 
                 EditorGUI.indentLevel++;
-                foreach (var asset in _subAssets)
+                foreach (Object asset in _subAssets)
                 {
-                    var r = EditorGUILayout.GetControlRect(true);
+                    Rect r = EditorGUILayout.GetControlRect(true);
 
                     r.width -= 60;
-                    var rField = new Rect(r);
+                    Rect rField = new(r);
                     if (_isRenaming)
                     {
                         rField.width = 12;
@@ -280,9 +279,9 @@ namespace Coffee.Editors
                 GUILayout.Toggle(true, "<b>Referencing Objects</b>", "IN Foldout");
                 EditorGUI.indentLevel++;
                 EditorGUILayout.HelpBox("Sub assets are excluded.", MessageType.None);
-                foreach (var asset in _referencingAssets.Except(_subAssets))
+                foreach (Object asset in _referencingAssets.Except(_subAssets))
                 {
-                    var r = EditorGUILayout.GetControlRect();
+                    Rect r = EditorGUILayout.GetControlRect();
 
                     r.width -= 20;
                     EditorGUI.ObjectField(r, asset, asset.GetType(), false);
@@ -295,7 +294,7 @@ namespace Coffee.Editors
                     // Add object to sub asset.
                     if (GUI.Button(r, _contentAdd, EditorStyles.label))
                     {
-                        var addAsset = asset;
+                        Object addAsset = asset;
                         EditorApplication.delayCall += () => AddSubAsset(addAsset);
                     }
                 }
@@ -318,11 +317,11 @@ namespace Coffee.Editors
         private void DrawImportArea()
         {
             GUILayout.Space(5);
-            var dropArea = GUILayoutUtility.GetRect(0, 20, GUILayout.ExpandWidth(true), GUILayout.ExpandHeight(false));
+            Rect dropArea = GUILayoutUtility.GetRect(0, 20, GUILayout.ExpandWidth(true), GUILayout.ExpandHeight(false));
             GUI.Box(dropArea, _contentImport, EditorStyles.helpBox);
 
-            var id = GUIUtility.GetControlID(FocusType.Passive);
-            var evt = Event.current;
+            int id = GUIUtility.GetControlID(FocusType.Passive);
+            Event evt = Event.current;
             switch (evt.type)
             {
                 case EventType.DragUpdated:
@@ -351,10 +350,10 @@ namespace Coffee.Editors
         /// </summary>
         private void ExportSubAsset(Object obj)
         {
-            var exportDir = Path.GetDirectoryName(AssetDatabase.GetAssetPath(_current));
-            var exportName = obj.name + " (Exported)." + GetFileExtension(obj);
-            var uniquePath = AssetDatabase.GenerateUniqueAssetPath(Path.Combine(exportDir, exportName));
-            AssetDatabase.CreateAsset(Object.Instantiate(obj), uniquePath);
+            string exportDir = Path.GetDirectoryName(AssetDatabase.GetAssetPath(_current));
+            string exportName = obj.name + " (Exported)." + GetFileExtension(obj);
+            string uniquePath = AssetDatabase.GenerateUniqueAssetPath(Path.Combine(exportDir, exportName));
+            AssetDatabase.CreateAsset(Instantiate(obj), uniquePath);
             AssetDatabase.SaveAssets();
             AssetDatabase.Refresh();
         }
@@ -400,39 +399,47 @@ namespace Coffee.Editors
 
         private void DeleteUnusedAll()
         {
-            foreach (var asset in _subAssets)
+            foreach (Object asset in _subAssets)
             {
                 if (!_referencingAssets.Contains(asset))
                 {
                     DeleteSubAsset(asset);
                 }
             }
+            AssetDatabase.SaveAssets();
+            AssetDatabase.Refresh();
+            OnSelectionChanged(Selection.activeObject);
         }
         private void ShowAll()
         {
-            var active = Selection.activeObject;
-            var assetPath = AssetDatabase.GetAssetPath(active);
-            foreach (var asset in AssetDatabase.LoadAllAssetsAtPath(assetPath).Where(x => x != null && !(x is GameObject || x is Component) && x != _current).Distinct())
+            Object active = Selection.activeObject;
+            string assetPath = AssetDatabase.GetAssetPath(active);
+            foreach (Object asset in AssetDatabase.LoadAllAssetsAtPath(assetPath).Where(x => x != null && !(x is GameObject || x is Component) && x != _current).Distinct())
             {
-                asset.hideFlags = HideFlags.None;
+                if (asset.hideFlags != HideFlags.None)
+                {
+                    asset.hideFlags = HideFlags.None;
+                    EditorUtility.SetDirty(asset);
+                }
             }
-            AssetDatabase.Refresh();
             AssetDatabase.SaveAssets();
+            AssetDatabase.Refresh();
+            OnSelectionChanged(active);
         }
 
         private void ShowPart()
         {
-            foreach (var asset in _subAssets)
+            foreach (Object asset in _subAssets)
             {
                 if (asset.GetType() == typeof(AnimatorStateTransition) || asset.GetType() == typeof(BlendTree) || asset.GetType() == typeof(AvatarMask) || asset.GetType() == typeof(AnimatorStateMachine) || asset.GetType() == typeof(AnimatorState) || asset.GetType().IsSubclassOf(typeof(StateMachineBehaviour)))
                 {
-                    SerializedObject so = new SerializedObject(asset);
-                    var sp = so.GetIterator();
+                    SerializedObject so = new(asset);
+                    SerializedProperty sp = so.GetIterator();
 
                     // Search referencing asset in SerializedProperties.
                     while (sp.Next(true))
                     {
-                        if(sp.propertyPath== "m_ObjectHideFlags")
+                        if (sp.propertyPath == "m_ObjectHideFlags")
                         {
                             sp.intValue = 1;
                         }
@@ -445,8 +452,9 @@ namespace Coffee.Editors
                     Debug.Log(asset.GetType());
                 }
             }
-            AssetDatabase.Refresh();
             AssetDatabase.SaveAssets();
+            AssetDatabase.Refresh();
+            OnSelectionChanged(Selection.activeObject);
         }
     }
 }
